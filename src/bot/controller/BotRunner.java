3eeed6663ca998492e2fae8da52dac0391873c47
denonbarton <dbar0540@740s-pl-126.local>
2015-12-16 12:11:02 -0700

    Started runner

---
+++ src/bot/controller/BotRunner.java
@@ -0,0 +1,12 @@
+package bot.controller;
+
+import bot.controller.BotController;
+
+public class BotRunner
+{
+	public static void main (String [] args)
+	{
+		BotController BotController = new BotController();
+		BotController.start();
+	}
+}
